43b2b9f87e521ecf1e1eb02851e514ea42cf1002
lutersergei <luter.sergei@ya.ru>
2016-08-22 13:56:55 +0700

    Add constructor AddUser

---
+++ users.php
@@ -0,0 +1,17 @@
+<?php
+//TODO Добавить работу с базой данных
+?>
+{
+  "status": "ok",
+  "users": [{
+    "id": "1",
+    "username": "admin",
+    "role": "10",
+    "active": "1"
+  }, {
+    "id": "2",
+    "username": "user",
+    "role": "20",
+    "active": "0"
+  }]
+}
